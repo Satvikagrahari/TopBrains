65a0e6216c5f7d93227155a24a4090ae0ed6d427
Satvik <agraharisatvik43@gmail.com>
2026-01-22 12:02:16 +0530

    Ques 10 Display Height

---
+++ 10_DisplayHeight/Program.cs
@@ -0,0 +1,37 @@
+﻿// Given a person's height in centimeters, return the height category:
+// - "Short"  : height < 150
+// - "Average": 150 <= height < 180
+// - "Tall"   : height >= 180
+
+// Input: heightCm (int)
+// Output: category (string)
+
+// Constraints:
+// 0 <= heightCm <= 300
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("enter height in cm = ");
+        int cm = int.Parse(Console.ReadLine());
+        if (cm < 150)
+        {
+            System.Console.WriteLine("Short");
+        } 
+        else if(cm>=150 && cm < 180)
+        {
+            System.Console.WriteLine("Average");
+        }
+        else if (cm >= 180)
+        {
+            System.Console.WriteLine("Tall");
+        }
+        else
+        {
+            System.Console.WriteLine("Out of range ");
+        }
+        
+    }
+}
